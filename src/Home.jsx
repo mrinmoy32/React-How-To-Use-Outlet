44dbcddc47061c4f05053ce1d2e80f94b3567388
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-02-27 22:08:42 +0530

    Home

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import { Outlet, Link } from 'react-router-dom';
+import Featured from './Featured';
+import Recent from './Recent';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <nav>
+        <ul>
+          <li><Link to="featured">Featured</Link></li>
+          <li><Link to="recent">Recent</Link></li>
+        </ul>
+      </nav>
+      <Outlet /> {/* Child routes will be rendered here */}
+    </div>
+  );
+};
+
+export default Home;
