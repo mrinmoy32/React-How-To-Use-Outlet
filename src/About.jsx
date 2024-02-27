998ee19f2f310eecc545131fd3221448026ef088
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-02-27 22:09:19 +0530

    About

---
+++ src/About.jsx
@@ -0,0 +1,20 @@
+import { Outlet, Link } from 'react-router-dom';
+import Team from './Team';
+import History from './History';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <nav>
+        <ul>
+          <li><Link to="team">Team</Link></li>
+          <li><Link to="history">History</Link></li>
+        </ul>
+      </nav>
+      <Outlet /> {/* Child routes will be rendered here */}
+    </div>
+  );
+};
+
+export default About;
