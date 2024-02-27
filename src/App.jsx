f76c756733a5f97a15e8aff47bb2a2403d8ac64d
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-02-27 22:06:26 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 import './App.css'
+
+/*In React Router, <Outlet /> is a component that acts as a placeholder where child routes can be rendered.
+
+When you have nested routes in React Router, the parent route's component can include 
+the <Outlet /> component to specify the location where the child routes should be rendered. 
+<Outlet /> indicates the location in your component where the content of child routes will be injected.
+*/
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+      <Routes>
+        <Route path="/home/*" element={<Home />} />
+        <Route path="/about/*" element={<About />} />
+      </Routes>
+    </Router>
     </>
   )
 }
